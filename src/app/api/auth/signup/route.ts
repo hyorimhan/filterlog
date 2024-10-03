adc711ea9b20af62ff5fd251c21fd953465b3d72
hyorimhan <mynameishan8@gmail.com>
2024-10-03 19:03:17 +0900

    fix: signup 수정

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,18 +1,29 @@
 import { createClient } from '@/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
-const supabase = createClient();
 
 export async function POST(request: NextRequest) {
+  const supabase = createClient();
+
   try {
-    const { email, password } = await request.json();
-    const { error } = await supabase.auth.signUp({ email, password });
+    const { email, password, nickname } = await request.json();
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: { data: { display_name: nickname } },
+    });
+
+    // const { error: nickError } = await supabase.from('user').insert('nickname');
     if (error) {
       if (error.message.includes('already registered')) {
         return NextResponse.json({ message: '이미 가입된 이메일입니다' });
       }
+      console.log(error.message);
       return NextResponse.json({ message: '회원가입에 실패했습니다' });
     }
-    return NextResponse.json({ Message: '회원가입에 성공했습니다' });
+    // if (nickError) {
+    //   return NextResponse.json({ message: nickError.message });
+    // }
+    return NextResponse.json({ message: '회원가입에 성공했습니다' });
   } catch (error) {
     return NextResponse.json({ message: '네트워크 오류가 발생했습니다' });
   }
--- src/service/auth.ts
+++ src/service/auth.ts
@@ -2,17 +2,37 @@ import { createClient } from '@/supabase/client';
 import { Database } from '@/types/supabase';
 import axios from 'axios';
 
-const supabase = createClient();
-
 export const sIgnUp = async ({
   email,
   password,
+  nickname,
 }: Database['public']['Tables']['users']['Insert'] & { password: string }) => {
-  const response = await axios.post('/api/auth/signup', { email, password });
-  return response.data;
+  // const response = await axios.post('/api/auth/signup', {
+  //   email,
+  //   password,
+  //   nickname,
+  // });
+  // return response.data;
+  try {
+    const response = await axios.post('/api/auth/signup', {
+      email,
+      password,
+      nickname,
+    });
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Axios 에러:', error.response?.data || error.message);
+    } else {
+      console.error('알 수 없는 에러:', error);
+    }
+    throw error;
+  }
 };
 
 export const nicknameConfirm = async () => {
+  const supabase = createClient();
+
   const { data: nickname } = await supabase.from('users').select('*');
   return nickname;
 };
